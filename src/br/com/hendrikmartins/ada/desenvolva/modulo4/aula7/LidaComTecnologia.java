3007bcebd5862d4ff55396669aa7df5b26fb9396
Hendrik <136930799+hendrikmartins@users.noreply.github.com>
2025-02-24 21:56:32 -0300

    Subindo arquivos da maquina

---
+++ src/br/com/hendrikmartins/ada/desenvolva/modulo4/aula7/LidaComTecnologia.java
@@ -0,0 +1,9 @@
+package br.com.hendrikmartins.ada.desenvolva.modulo4.aula7;
+
+
+public class LidaComTecnologia extends Funcionario {
+
+    void programar(){
+        System.out.println("Programando...");
+    }
+}
